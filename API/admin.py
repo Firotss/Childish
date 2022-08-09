f7ec257663f126c7af434e84723c3830c69b2d9f
Firotss <jannadom8@gmail.com>
2022-08-09 08:44:34 +0300

    Add register model in django admin

--- API/admin.py
+++ API/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from .models import Product, Order
 
-@admin.register(Product)
-class RequestDemoAdmin(admin.ModelAdmin):
-  list_display = [field.name for field in
-Product._meta.get_fields()]
-
-# @admin.register(Order)
-# class RequestDemoAdmin(admin.ModelAdmin):
-#   list_display = [field.name for field in
-# Order._meta.get_fields()]
+admin.site.register(Product)
+admin.site.register(Order)
